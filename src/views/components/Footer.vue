6c7a3a9e60dd36bd087d9fb3c5dcc41e3b2afe16
aisampletype01-sudo <ai.sample.type01@gmail.com>
2025-09-10 15:56:09 +0000

    test

---
+++ src/views/components/Footer.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref, watchEffect } from 'vue'
+const props = defineProps({
+    title: String
+})
+watchEffect(() => {
+  console.log(props.title)
+})
+</script>
+
+<template>
+  footer
+</template>
+
+<style lang="scss" scoped>
+</style>
--- src/views/pages/main.vue
+++ src/views/pages/main.vue
@@ -1,5 +1,6 @@
 <script setup>
 import Header from '../components/Header.vue'
+import Footer from '../components/Footer.vue'
 </script>
 
 <template>
@@ -7,6 +8,7 @@ import Header from '../components/Header.vue'
   <div class="p-contentBody">
     main
   </div>
+  <Footer />
 </template>
 
 <style lang="scss" scoped>
