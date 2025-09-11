61a8896cfaf4c1dfa1093dbd7f8016a1c295f95d
aisampletype01-sudo <ai.sample.type01@gmail.com>
2025-09-11 17:34:21 +0000

    빈화면 처리 완료

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,12 @@
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/dev/'
-    : '/'
-}
-
 // module.exports = {
-//   publicPath: process.env.NODE_ENV === 'production' 
-//     ? './' 
+//   publicPath: process.env.NODE_ENV === 'production'
+//     ? '/dev/'
 //     : '/'
-//   //publicPath: process.env.NODE_ENV === 'production' ? '/이곳에 경로를 적으시오./' : '/'
-// };
+// }
+
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' 
+    ? './' 
+    : './'
+  //publicPath: process.env.NODE_ENV === 'production' ? '/이곳에 경로를 적으시오./' : '/'
+};
